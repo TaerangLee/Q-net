d9d7e3c2f3e7d8c1068c2a5bcef2cb7e5acd2f88
TAERANG <s22051@gsm.hs.kr>
2022-12-11 01:01:12 +0900

    renew

--- Q_NET/Q net.js
+++ Q_NET/Q net.js
@@ -1,3 +1,11 @@
 const test = document.querySelector(".Customer_Engagement");
 
 console.log(test);
+
+function add1(){
+    console.log("click here");
+    test.style.color="blue";
+
+}
+
+test.addEventListener("click", add1);
